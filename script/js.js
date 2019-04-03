0df8e6ccf3f6d20806cd42cc26f11ca649a8e3de
iamceltic33 <39075620+iamceltic33@users.noreply.github.com>
2019-04-03 11:40:34 +0600

    Update js.js

--- script/js.js
+++ script/js.js
@@ -1,5 +1,21 @@
 $(document).ready( function(){
    let images = ["images/bg.png", "images/bg2.png", "images/bg3.png", "images/bg4.png", "images/bg5.png"];
+   let loadedImages = images.map(function(val, index){
+      let img = new Image();
+      img.src = val;
+      $(img).on('load', function(){
+         images[index] = `url("${$(this).attr('src')}")`;
+         console.log(images[index])
+         $('.header').css({
+            backgroundImage: images[index],
+            backgroundSize: 'cover'});
+      })
+      return img
+   });
+   images = images.map(function(){
+      return 'none'
+   })
+   console.log(loadedImages);
    let slider = new Slider(images, $('.header'), $('.switchers'));
    slider.changeBG();
    slider.slideShowStart(5000);
@@ -12,6 +28,7 @@ $(document).ready( function(){
       e.preventDefault();
       $('nav').slideUp(300)
    })
+   $(window).on('mousemove', fillGradOnMove)
 })
 class Slider {
    constructor(images, target, switchers) {
@@ -33,10 +50,9 @@ class Slider {
       }
    };
    changeBG(){
-      console.log(this);
-      let url = this.images[this.counter]
+      let url = this.images[this.counter];
       this.target.css({
-         backgroundImage: `url("${url}")`,
+         backgroundImage: url,
          backgroundSize: 'cover'});
       this.counterUp();
       this.switchers.children('.active').removeClass('active');
@@ -46,10 +62,10 @@ class Slider {
       this.timerID = setInterval(this.changeBG.bind(this), interval)
    }
 }
-// let slider = {
-//    counter: 0,
-//    ,
 
+function fillGradOnMove(e){
+   let x = e.pageX - $('.menu').offset().left,
+      y = e.pageY - $('.menu').offset().top;
+      $('.menu').css('background', `radial-gradient(circle at ${100*x/$(this).width()}% ${100*y/$(this).height()}%, rgba(0,0,0,0.95) 0%, rgba(84,84,84,0.99) 100%)`)
 
-
-// }
+}
